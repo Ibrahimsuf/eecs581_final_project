34a9b9b916dff969ad1193f0ec0dcc6d79bb0493
Ibrahimsuf <110345830+Ibrahimsuf@users.noreply.github.com>
2025-11-08 09:13:03 -0600

    create socket

--- project/app.py
+++ project/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
-
+from flask_sock import Sock
 app = Flask(__name__)
-
+sock = Sock(app)
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -11,6 +11,14 @@ def get_jobs():
     skills = request.form['skills']
     # For now, just test the connection
     return jsonify({"message": f"Received skills: {skills}"})
+
+@sock.route('/job_socket')
+def websocket(ws):
+    while True:
+        data = ws.receive()
+        if data is None:
+            break
+        ws.send(f"Echo: {data}")
 
 if __name__ == '__main__':
     app.run(debug=True)
